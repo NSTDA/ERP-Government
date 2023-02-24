3c022c31a3a2763b17c9de575f7fe4940cedf2cc
Saran440 <saranl@ecosoft.co.th>
2023-02-24 14:58:52 +0700

    [FIX] refuse expense at state draft

--- odoo/custom/src/private/egov_hr_expense/models/hr_expense.py
+++ odoo/custom/src/private/egov_hr_expense/models/hr_expense.py
@@ -12,3 +12,19 @@ class HrExpense(models.Model):
         res = super()._get_under_validation_exceptions()
         res.extend(["date_commit"])
         return res
+
+
+class HrExpenseSheet(models.Model):
+    _inherit = "hr.expense.sheet"
+
+    def refuse_sheet(self, reason):
+        """Allow refuse with state draft, no permission"""
+        if self._context.get("self_refuse", False):
+            self.write({"state": "cancel"})
+            for sheet in self:
+                sheet.message_post_with_view(
+                    "hr_expense.hr_expense_template_refuse_reason",
+                    values={"reason": reason, "is_sheet": True, "name": sheet.name},
+                )
+            return self.activity_update()
+        return super().refuse_sheet(reason)
