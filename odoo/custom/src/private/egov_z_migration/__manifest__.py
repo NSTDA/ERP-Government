48bc6cc3dbbd0e434e509c3604566ab7dfc07cff
ps-tubtim <pimolnats@ecosoft.co.th>
2023-06-06 09:15:29 +0700

    [ADD] egov_z_migration

---
+++ odoo/custom/src/private/egov_z_migration/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "EGOV - Migration Script",
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "EGOV",
+    "author": "Ecosoft",
+    "depends": [
+        "account_asset_management",
+    ],
+    "data": [
+        "views/account_asset_views.xml",
+    ],
+}
